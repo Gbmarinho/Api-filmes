2cf131e11270001d5b1d9a9bf6cf403ad69d38a6
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-03-31 13:09:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+const privateKey = "7cd3684824a067744989aa33c44a0fefb24a8740";
+const publicKey = "22e9bab7b462ebbd01fee470d5c30192";
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+
+   </div>
+  )
+}
+
+export default App
